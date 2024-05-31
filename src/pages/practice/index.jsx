92dd3cfe65ffb53e1271b6def26942229941f947
Bendaa08 <benedikt.student@gmail.com>
2024-05-31 15:36:26 +0200

    feat: frontend work

--- src/pages/practice/index.jsx
+++ src/pages/practice/index.jsx
@@ -23,11 +23,14 @@ export default function Practice() {
     return (
         <div>
             {jsonData ? (
-                <div className='flex items-center'>
-                    <p className='m-[0px] mr-[5px]'>{jsonData[0].first_part}</p>
-                    <textarea className='resize-none' autocapitalize="off" autocomplete="off" autocorrect="off" spellcheck="false" lang="cs" placeholder="Pište zde"></textarea>
-                    <p className='m-[0px]'>{jsonData[0].last_part}</p>
-                </div >
+                <>
+                    <div className='flex items-center mb-[25vh] h-[55vh] justify-center'>
+                        <p className='m-[0px] mr-[10px]'>{jsonData[0].first_part}</p>
+                        <textarea className="resize-none overflow-hidden" autoCapitalize="off" autoFocus="on" rows="1" autoComplete="off" autoCorrect="off" type="text" spellCheck="false" lang="cs" placeholder="Pište zde"></textarea>
+                        <p className='m-[0px] ml-[10px]'>{jsonData[0].last_part}</p>
+                    </div >
+                    <button className='text-[28px] bg-transparent border-[5px] border-red px-4 py-2 text-white hover:bg-white hover:text-black ml-[80vh] h-[50px]'>odeslat</button>
+                </>
             ) : (
                 <p>Loading...</p>
             )
