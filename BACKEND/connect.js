f12d7bc8d30fda25cdaa49707893dc9b61ac569c
Bendaa08 <91610521+bendaacz@users.noreply.github.com>
2024-06-10 18:51:41 +0200

    feat: save login method for database

--- BACKEND/connect.js
+++ BACKEND/connect.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql')
+require('dotenv').config()
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "czuolingo"
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASS,
+    database: process.env.DB
 })
 
 module.exports = db;
