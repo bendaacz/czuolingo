1c488210923cd31da601eae3485fc8df10cc17e0
Bendaa08 <91610521+bendaacz@users.noreply.github.com>
2024-06-19 17:26:12 +0200

    main page fix

--- src/pages/start/index.jsx
+++ src/pages/start/index.jsx
@@ -5,24 +5,21 @@ export default function Start() {
     return (
         <div className="w-90 h-[190vh]">
             <div className=""> {/* add different bg color maybe. */}
-                <div className="border-black border-b-[1px] p-[2vh] h-[8vh] justify-between flex px-[20vh] fixed top-0 left-0 right-0">
+                <div className="border-black border-b-[1px] p-[1.5vh] h-[9.1vh] justify-between inline-block px-[20vh] fixed top-0 left-0 right-0">
                     <div className="flex w-full justify-between items-center">
-                        <div className="flex justify-between w-[30vh]">
+                        <div className="flex text-[2vw] justify-between w-[40vh]">
                             <p>item1</p>
+                            <p>/</p>
                             <p>item2</p>
+                            <p>/</p>
                             <p>item3</p>
-                        </div>
-                        <div className="w-[30vh] flex justify-between">
-                            <p>item4</p>
-                            <p>item5</p>
-                            <p>item6</p>
                         </div>
                     </div>
                 </div>
-                <h2 className="text-[200px] mt-[5vh] flex border-black border-b-[1px]">czuolingo</h2>
-                <div className="flex">
+                <h2 className="text-[20vw] mt-[5vh] inline-block font-inter border-r-[1px] border-black border-b-[1px]">asdfghjkl</h2>
+                <div className="inline-block">
                     <Link to="/practice">
-                        <p className="text-[#2824ff] text-[50px] border-black border-b-[1px] border-r-[1px]">Chci 50 bodů z ČJ</p>
+                        <p className="text-[#2824ff] text-[2.6vw] border-black border-b-[1px] border-r-[1px]">Chci 50 bodů z ČJ</p>
                     </Link>
                 </div>
             </div>
