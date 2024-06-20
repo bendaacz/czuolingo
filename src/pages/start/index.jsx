1d859c37778727f00d4aff8c26c44ed5947cc908
Bendaa08 <91610521+bendaacz@users.noreply.github.com>
2024-06-20 17:25:44 +0200

    main page improvements

--- src/pages/start/index.jsx
+++ src/pages/start/index.jsx
@@ -3,24 +3,32 @@ import { Link } from "react-router-dom";
 
 export default function Start() {
     return (
-        <div className="w-90 h-[190vh]">
+        <div className="h-[190vh]">
             <div className=""> {/* add different bg color maybe. */}
                 <div className="border-black border-b-[1px] p-[1.5vh] h-[9.1vh] justify-between inline-block px-[20vh] fixed top-0 left-0 right-0">
                     <div className="flex w-full justify-between items-center">
-                        <div className="flex text-[2vw] justify-between w-[40vh]">
-                            <p>item1</p>
+                        <div className="flex text-[38px] justify-between w-[55vh]">
+                            <Link to="/practice"> {/*temp link !!!!*/}
+                                <p>učit se</p>
+                            </Link>
                             <p>/</p>
-                            <p>item2</p>
+                            <p>kontakt</p>
                             <p>/</p>
-                            <p>item3</p>
+                            <p>o nás</p>
                         </div>
                     </div>
                 </div>
-                <h2 className="text-[20vw] mt-[5vh] inline-block font-inter border-r-[1px] border-black border-b-[1px]">asdfghjkl</h2>
-                <div className="inline-block">
-                    <Link to="/practice">
-                        <p className="text-[#2824ff] text-[2.6vw] border-black border-b-[1px] border-r-[1px]">Chci 50 bodů z ČJ</p>
-                    </Link>
+                <div className="flex">
+                    <h2 className="text-[15vw] mt-[9.1vh] inline-block font-inter border-r-[1px] border-black border-b-[1px]">asdfghjkl</h2>
+                    <p className="w-[32vw] mt-[9.1vh] text-[2.8vw] p-[1vw] flex text-center items-center border-black border-b-[1px] break-words">Učte se Český jazyk a všechny jeho nástrahy s jednou aplikací a 5 minutami denně.</p>
+                </div>
+
+                <div className="">
+                    <div className="inline-block">
+                        <Link to="/practice">
+                            <p className="text-[#2824ff] text-[2.6vw] border-black border-b-[1px] border-r-[1px]">Chci 50 bodů z ČJ</p>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
