b2e0979ce74e75b4b3c0225b14d6124b703c3fe6
Bendaa08 <91610521+bendaacz@users.noreply.github.com>
2024-06-21 17:40:42 +0200

    feat: made loader ready to use

---
+++ src/pages/loading/index.jsx
@@ -0,0 +1,9 @@
+import "./index.css";
+
+export default function Loading() {
+    return (
+        <div className="w-[200vh] h-[200vh]">
+            <div className="load"></div>                {/* currently NOT IN USE */}
+        </div>
+    )
+}
