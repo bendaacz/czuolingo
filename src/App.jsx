368d17b8a8eb620191bef9914c6d88b90e95f26a
Bendaa08 <91610521+bendaacz@users.noreply.github.com>
2024-06-23 18:28:36 +0200

    perf: added /prislovi1 path

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Start from "./pages/start/index";
-import Practice from "./pages/practice/index";
-
+import Prislovi1 from "./pages/exercises/prislovi1/index";
 import "./index.css";
+import Practice from './pages/practice';
 
 export default function App() {
     return (
@@ -11,6 +11,7 @@ export default function App() {
                 <Routes>
                     <Route path="/" element={<Start />} />
                     <Route path='/practice' element={<Practice />} />
+                    <Route path='/prislovi1' element={<Prislovi1 />} />
                 </Routes>
             </BrowserRouter>
         </>
