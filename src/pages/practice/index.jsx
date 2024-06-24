26c2ad27ffd9daba423ada128336cb243394f1ee
Bendaa08 <91610521+bendaacz@users.noreply.github.com>
2024-06-24 17:53:57 +0200

    perf: moved login queries to login folder

--- src/pages/practice/index.jsx
+++ src/pages/practice/index.jsx
@@ -1,31 +1,10 @@
 import { useEffect, useState } from "react"
 
 export default function Practice() {
-
-    const [username, setUsername] = useState("bendaa");
-    const [hash, setHash] = useState(null);
-
-    useEffect(() => {
-        const getHash = async () => {
-            try {
-                const response = await fetch('http://localhost:5174/api/hash/' + username);
-                if (!response.ok) {
-                    throw new Error('Odezva serveru nebyla OK.');
-                }
-                const data = await response.json();
-                setHash(data.hash);
-            } catch (error) {
-                console.error('Problém při stahování dat:', error);
-            }
-        };
-
-        getHash();
-    }, []);
-
     return (
         <>
             <p>Practice</p>
-            <div className="">{hash}</div>
+            <div className=""></div>
         </>
     )
 }
