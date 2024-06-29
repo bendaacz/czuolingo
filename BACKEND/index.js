704a2c333c6a9936326cdae66f1a4b2dff73b59b
Bendaa <benedikt.student@gmail.com>
2024-06-29 19:47:38 +0200

    feat: added API endpoints for practice page

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -34,33 +34,59 @@ app.get("/api/prislovi1_id", (req, res) => {
   );
 });
 
+app.get("/api/exercises/list", (req, res) => {
+  db.query("SELECT id, name, questions FROM exercise", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: "Internal server error" });
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.get("/api/random/prislovi1", (req, res) => {
+  db.query(
+    "SELECT first_part, last_part FROM prislovi1 ORDER BY RAND() LIMIT 3;",
+    (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ error: "Internal server error" });
+      } else {
+        res.json(data);
+      }
+    }
+  );
+});
+
 app.get("/api/hash/:hash", (req, res) => {
-  require('dotenv').config();
-  const crypto = require('crypto');
-  const secret = (process.env.HASH);
-  const update = (req.params.hash)
-  const hash = crypto.createHash('sha256', secret)
-    .update(update)
-    .digest('hex');
+  require("dotenv").config();
+  const crypto = require("crypto");
+  const secret = process.env.HASH;
+  const update = req.params.hash;
+  const hash = crypto.createHash("sha256", secret).update(update).digest("hex");
   res.json({ hash });
 });
 
 app.post("/login", (req, res) => {
   const { username, hash } = req.body;
-  const loginQuery = "SELECT * FROM users WHERE username = ? AND hash = ?"
+  const loginQuery = "SELECT * FROM users WHERE username = ? AND hash = ?";
   db.query(loginQuery, [username, hash], (err, result) => {
     if (err) {
-      res.status(500).json({ message: 'An error occurred while processing your request.' });
+      res
+        .status(500)
+        .json({ message: "An error occurred while processing your request." });
     } else {
       if (result.length > 0) {
-        res.status(200).json({ message: 'Login successful' });
+        res.status(200).json({ message: "Login successful" });
       } else {
-        res.status(401).json({ message: 'Login failed. Invalid username or password.' });
+        res
+          .status(401)
+          .json({ message: "Login failed. Invalid username or password." });
       }
     }
   });
-})
-
+});
 
 app.listen(PORT, () => {
   console.log(`bezim na ${PORT}`);
