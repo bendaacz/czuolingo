504a1efa97a93960deb5dac19fa4eb0a25345e3b
Bendaa <benedikt.student@gmail.com>
2024-06-30 19:59:46 +0200

    working on fully dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Start from "./pages/start/index";
-import Prislovi1 from "./pages/exercises/prislovi1/index";
 import LogIn from "./pages/login/index";
 import "./index.css";
 import Practice from './pages/practice';
+import Exercise from './pages/exercises/index';
 
 export default function App() {
     return (
@@ -13,7 +13,7 @@ export default function App() {
                     <Route path="/" element={<Start />} />
                     <Route path="/login" element={<LogIn />} />
                     <Route path='/practice' element={<Practice />} />
-                    <Route path='/prislovi1' element={<Prislovi1 />} />
+                    <Route path='/exercise/prislovi1' element={<Exercise exerciseName="prislovi1" />} />
                 </Routes>
             </BrowserRouter>
         </>
