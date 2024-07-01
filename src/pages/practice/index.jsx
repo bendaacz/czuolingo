c2964a69a294c8f643483ed09709482f0b002408
Bendaa <benedikt.student@gmail.com>
2024-07-01 10:41:54 +0200

    fix: fixed small text

--- src/pages/practice/index.jsx
+++ src/pages/practice/index.jsx
@@ -3,9 +3,9 @@ import PopupPreview from "./Popup";
 export default function Practice() {
     return (
         <>
-        <div className="h-screen w-screen py-[10vh]">
+        <div className="h-screen w-screen text-[3vh] py-[10vh]">
             <div className="flex justify-center text-center align-middle">
-                <div className="grid grid-cols-5">
+                <div className="grid grid-cols-8">
                 <PopupPreview number="0" exerciseName="prislovi1" />
                 <PopupPreview number="1" exerciseName="prislovi2" />
                 </div>
